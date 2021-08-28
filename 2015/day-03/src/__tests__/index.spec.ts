676d535c19e3e298ee4afecf645be0458f3b5b85
David Fulton <david.fulton@lifestylepubs.com>
2021-08-28 15:37:35 -0500

    Completed part 1

--- 2015/day-03/src/__tests__/index.spec.ts
+++ 2015/day-03/src/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
-import { sum } from '../index';
+import { processSteps, stepsMap, currentStep, Step } from '../index';
 
-test('sum', () => {
-  expect(sum()).toBe(0);
+test('processSteps returns the correct num of houses', () => {
+  expect(processSteps('>' as unknown as Step[])).toBe(2);
 });
--- 2015/day-03/src/index.ts
+++ 2015/day-03/src/index.ts
@@ -1,5 +1,63 @@
-import { shout } from './utils';
+import { readFile, shout } from './utils';
 
-export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export type Step = '<' | '>' | '^' | 'v';
 
-shout('Hello world!');
+export const stepsMap: { [key: string]: number } = {
+  '0,0': 1,
+};
+
+export const currentStep = {
+  x: 0,
+  y: 0,
+
+  toStepKey() {
+    return `${this.x},${this.y}`;
+  },
+};
+
+export function processSteps(steps: Step[]): number {
+  for (const step of steps) {
+    processStep(step);
+  }
+
+  return Object.keys(stepsMap).length;
+}
+
+function processStep(step: Step) {
+  moveStep(step);
+  deliverPresent();
+}
+
+function deliverPresent() {
+  const stepKey = currentStep.toStepKey();
+  if (stepsMap.hasOwnProperty(stepKey)) {
+    stepsMap[stepKey] += 1;
+  } else {
+    stepsMap[stepKey] = 1;
+  }
+}
+
+function moveStep(step: Step) {
+  if (['<', '>'].includes(step)) {
+    moveX(step);
+  } else {
+    moveY(step);
+  }
+}
+
+function moveX(step: Step) {
+  const move = step === '>' ? 1 : -1;
+  currentStep.x += move;
+}
+
+function moveY(step: Step) {
+  const move = step === '^' ? 1 : -1;
+  currentStep.y += move;
+}
+
+async function main() {
+  const [steps] = await readFile();
+  shout(processSteps(steps as unknown as Step[]));
+}
+
+main();
