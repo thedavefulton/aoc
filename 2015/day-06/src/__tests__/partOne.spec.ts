0e62ddb9f31098ba19ec7a999ee3aca196033c54
David Fulton <david.fulton@lifestylepubs.com>
2021-08-29 18:23:23 -0500

    completed part one

--- 2015/day-06/src/__tests__/partOne.spec.ts
+++ 2015/day-06/src/__tests__/partOne.spec.ts
@@ -1,5 +1,65 @@
-import { sum } from '../partOne';
+import { Command, LightBoard } from '../partOne';
 
-test('sum', () => {
-  expect(sum()).toBe(0);
+describe('LightBoard', () => {
+  test('create LightBoard grid', () => {
+    const lb = new LightBoard(2);
+    expect(lb.grid).toStrictEqual([
+      [false, false],
+      [false, false],
+    ]);
+  });
+
+  test('parseInstruction', () => {
+    const lb = new LightBoard(2);
+    expect(lb.parseInstruction('turn on 0,0 through 999,999')).toStrictEqual({
+      command: 'TURN_ON',
+      topLeft: [0, 0],
+      botRight: [999, 999],
+    });
+    expect(lb.parseInstruction('toggle 0,0 through 999,0')).toStrictEqual({
+      command: 'TOGGLE',
+      topLeft: [0, 0],
+      botRight: [999, 0],
+    });
+    expect(lb.parseInstruction('turn off 499,499 through 500,500')).toStrictEqual({
+      command: 'TURN_OFF',
+      topLeft: [499, 499],
+      botRight: [500, 500],
+    });
+  });
+
+  test('toggle', () => {
+    const lb = new LightBoard(2);
+    lb.toggle({ command: Command.TOGGLE, topLeft: [0, 0], botRight: [0, 1] });
+    expect(lb.grid).toStrictEqual([
+      [true, true],
+      [false, false],
+    ]);
+    lb.toggle({ command: Command.TOGGLE, topLeft: [0, 0], botRight: [1, 1] });
+    expect(lb.grid).toStrictEqual([
+      [false, false],
+      [true, true],
+    ]);
+  });
+
+  test('turnOn', () => {
+    const lb = new LightBoard(2);
+    lb.turnOn({ command: Command.TURN_ON, topLeft: [0, 0], botRight: [0, 1] });
+    expect(lb.grid).toStrictEqual([
+      [true, true],
+      [false, false],
+    ]);
+    lb.turnOn({ command: Command.TURN_ON, topLeft: [0, 0], botRight: [1, 1] });
+    expect(lb.grid).toStrictEqual([
+      [true, true],
+      [true, true],
+    ]);
+  });
+
+  test('numLit', () => {
+    const lb = new LightBoard(2);
+    expect(lb.numLit).toBe(0);
+    lb.turnOn({ command: Command.TURN_ON, topLeft: [0, 0], botRight: [0, 1] });
+    expect(lb.numLit).toBe(2);
+  });
 });
--- 2015/day-06/src/partOne.ts
+++ 2015/day-06/src/partOne.ts
@@ -1,5 +1,115 @@
-import { shout } from './utils';
+import { readFile, shout } from './utils';
 
-export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export enum Command {
+  TOGGLE = 'TOGGLE',
+  TURN_OFF = 'TURN_OFF',
+  TURN_ON = 'TURN_ON',
+}
 
-shout('Hello world!');
+export interface Instruction {
+  command: Command;
+  topLeft: [number, number];
+  botRight: [number, number];
+}
+
+export class LightBoard {
+  grid: boolean[][];
+
+  constructor(dimension: number) {
+    this.grid = this.generateGrid(dimension);
+  }
+
+  private generateGrid(dimension: number): boolean[][] {
+    const rows: boolean[][] = [];
+    for (let i = 0; i < dimension; i++) {
+      rows.push(this.generateRow(dimension));
+    }
+
+    return rows;
+  }
+
+  private generateRow(dimension: number): boolean[] {
+    const row: boolean[] = [];
+    for (let i = 0; i < dimension; i++) {
+      row.push(false);
+    }
+
+    return row;
+  }
+
+  followInstruction(instruction: string) {
+    const parsedInstruction = this.parseInstruction(instruction);
+    if (parsedInstruction.command === Command.TOGGLE) {
+      this.toggle(parsedInstruction);
+    } else if (parsedInstruction.command === Command.TURN_ON) {
+      this.turnOn(parsedInstruction);
+    } else {
+      this.turnOff(parsedInstruction);
+    }
+  }
+
+  toggle(instruction: Instruction) {
+    for (let i = instruction.topLeft[0]; i <= instruction.botRight[0]; i++) {
+      for (let j = instruction.topLeft[1]; j <= instruction.botRight[1]; j++) {
+        this.grid[i][j] = !this.grid[i][j];
+      }
+    }
+  }
+
+  turnOn(instruction: Instruction) {
+    for (let i = instruction.topLeft[0]; i <= instruction.botRight[0]; i++) {
+      for (let j = instruction.topLeft[1]; j <= instruction.botRight[1]; j++) {
+        this.grid[i][j] = true;
+      }
+    }
+  }
+
+  turnOff(instruction: Instruction) {
+    for (let i = instruction.topLeft[0]; i <= instruction.botRight[0]; i++) {
+      for (let j = instruction.topLeft[1]; j <= instruction.botRight[1]; j++) {
+        this.grid[i][j] = false;
+      }
+    }
+  }
+
+  parseInstruction(instruction: string): Instruction {
+    const re = /([a-z\s]+)(\d+),(\d+) through (\d+),(\d+)/;
+    // @ts-ignore
+    const [, command, left, top, right, bot] = re.exec(instruction);
+
+    return {
+      command:
+        command.trim() === 'toggle'
+          ? Command.TOGGLE
+          : command.trim() === 'turn on'
+          ? Command.TURN_ON
+          : Command.TURN_OFF,
+      topLeft: [parseInt(left), parseInt(top)],
+      botRight: [parseInt(right), parseInt(bot)],
+    };
+  }
+
+  get numLit(): number {
+    let count = 0;
+    for (const row of this.grid) {
+      for (const light of row) {
+        if (light) count++;
+      }
+    }
+
+    return count;
+  }
+}
+
+async function main() {
+  const lb = new LightBoard(1000);
+  const instructions = await readFile();
+
+  for (const instruction of instructions) {
+    lb.followInstruction(instruction);
+  }
+
+  shout(lb.numLit);
+}
+
+main();
